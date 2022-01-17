4283c85a06e4298e0b22202c1772fd98fb6ae651
Craig Miller <craigmiller160@gmail.com>
2022-01-17 16:19:12 -0500

    Implemented another test

--- test/OptionExt.test.ts
+++ test/OptionExt.test.ts
@@ -1,7 +1,15 @@
-export {};
+import * as Option from 'fp-ts/Option';
+import * as OptionExt from '../src/OptionExt';
 
 describe('OptionExt', () => {
 	it('getOrNull', () => {
-		throw new Error();
+		const some = Option.some('Hello');
+		const none = Option.none;
+
+		const someValue = OptionExt.getOrNull(some);
+		expect(someValue).toEqual('Hello');
+
+		const noneValue = OptionExt.getOrNull(none);
+		expect(noneValue).toEqual(null);
 	});
 });
