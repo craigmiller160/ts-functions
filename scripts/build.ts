dfc0782230739179708f68c95e9ab141816536e1
Craig Miller <craigmiller160@gmail.com>
2022-01-17 10:01:21 -0500

    More progress with build script

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,16 +1,25 @@
 import spawn from 'cross-spawn';
+import path from 'path';
+import fs from 'fs';
 
 // TODO add validation to this
+
+interface PackageJson {
+    scripts: {
+        prepare?: string;
+    }
+}
 
 const build = () => spawn.sync('tsc', {
     stdio: 'inherit'
 });
 
-const copyFiles = () => {
-    spawn.sync('cp', ['package.json', 'lib'], {
-        cwd: process.cwd(),
-        stdio: 'inherit'
-    });
+const copyPackageJson = () => {
+    const packageJsonTxt = fs.readFileSync(path.join(process.cwd(), 'package.json'), 'utf8');
+    const packageJson = JSON.parse(packageJsonTxt) as PackageJson;
+    delete packageJson.scripts.prepare;
+    fs.writeFileSync(path.join(process.cwd(), 'lib', 'package.json'), JSON.stringify(packageJson, null, 2));
 };
 
 build();
+copyPackageJson();
