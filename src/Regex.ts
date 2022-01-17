e545f25555af24ee355f24d4e86a22a46f703dbf
Craig <craigmiller160@gmail.com>
2022-01-17 09:08:37 -0500

    Fixed lint errors

--- src/Regex.ts
+++ src/Regex.ts
@@ -1,12 +1,13 @@
 import * as Option from 'fp-ts/Option';
 import { match, when } from 'ts-pattern';
 
-export const capture = <T>(
-	regex: RegExp | string
-) => (text: string): Option.Option<T> =>
-	match(text)
-		.with(
-			when<string>((_) => RegExp(regex).test(_)),
-			(_) => Option.some(RegExp(regex).exec(_)?.groups as unknown as T)
-		)
-		.otherwise(() => Option.none);
+export const capture =
+	<T>(regex: RegExp | string) =>
+	(text: string): Option.Option<T> =>
+		match(text)
+			.with(
+				when<string>((_) => RegExp(regex).test(_)),
+				(_) =>
+					Option.some(RegExp(regex).exec(_)?.groups as unknown as T)
+			)
+			.otherwise(() => Option.none);
