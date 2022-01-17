b7622742c806310130ac21a3d3966a9ef72d7978
Craig <craigmiller160@gmail.com>
2022-01-17 16:03:45 -0500

    Stubbed out tests

---
+++ test/File.test.ts
@@ -0,0 +1,23 @@
+export {};
+
+describe('File', () => {
+	it('readFileSync', () => {
+		throw new Error();
+	});
+
+	it('existsSync', () => {
+		throw new Error();
+	});
+
+	it('rmIfExistsSync', () => {
+		throw new Error();
+	});
+
+	it('mkdirSync', () => {
+		throw new Error();
+	});
+
+	it('listFilesSync', () => {
+		throw new Error();
+	});
+});
