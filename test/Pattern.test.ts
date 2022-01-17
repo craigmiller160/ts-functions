4286abcb886018569ecb78c2028994828c4bdedf
Craig Miller <craigmiller160@gmail.com>
2022-01-17 16:28:26 -0500

    Implemented test

--- test/Pattern.test.ts
+++ test/Pattern.test.ts
@@ -1,7 +1,17 @@
-export {};
+import { match } from 'ts-pattern';
+import * as Pattern from '../src/Pattern';
+
+const URL = 'https://www.google.com/foo/bar?abc=def&ghi=jkl';
+const URL_REGEX = /^https?:\/\/(?<hostname>.*?)\/.*\?(?<query>.*)$/;
+
+const doMatchPattern = (value: string): string =>
+	match(value)
+		.with(Pattern.regex(URL_REGEX), () => 'Success')
+		.otherwise(() => 'Failure');
 
 describe('Pattern', () => {
 	it('regex', () => {
-		throw new Error();
+		expect(doMatchPattern(URL)).toEqual('Success');
+		expect(doMatchPattern('abc')).toEqual('Failure');
 	});
 });
