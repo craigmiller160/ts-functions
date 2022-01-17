45205bc30a339e2ea65654d63dd3d2d0257d6fe6
Craig <craigmiller160@gmail.com>
2022-01-17 08:56:30 -0500

    More test setup

--- test/Regex.test.ts
+++ test/Regex.test.ts
@@ -1,10 +1,18 @@
 import '@relmify/jest-fp-ts';
 
+const URL = 'https://www.google.com/foo/bar?abc=def&ghi=jkl';
+const URL_NO_QUERY = 'https://www.google.com/foo/bar';
+const URL_REGEX = /^https?:\/\/(?<hostname>.*)\/.*\??(?<query>.*)$/;
+
 describe('Regex', () => {
 	describe('captureFromRegex', () => {
 		it('has match', () => {
 			throw new Error();
 		});
+
+		it('has match not all groups', () => {
+			throw new Error()
+		})
 
 		it('has no match', () => {
 			throw new Error();
