d3729c6cc276129c8af88fb799dab327b4fd4da6
Craig <craigmiller160@gmail.com>
2022-01-17 13:57:14 -0500

    Implemented a test

--- test/unknownToError.test.ts
+++ test/unknownToError.test.ts
@@ -1,8 +1,14 @@
+import { unknownToError } from '../src/unknownToError';
+
 export {};
 
 describe('unknownToError', () => {
 	it('handles Error', () => {
-		throw new Error();
+		const error = new Error('FooBar');
+		error.name = 'SpecialError';
+		const result = unknownToError(error);
+		expect(result).toEqual(error);
+		expect(result.name).toEqual(error.name);
 	});
 
 	it('handles other value', () => {
