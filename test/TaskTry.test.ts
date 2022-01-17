accf5f217ee5641061376b47490a0cfb2c5b6e13
Craig <craigmiller160@gmail.com>
2022-01-17 14:33:36 -0500

    Implemented more tests

--- test/TaskTry.test.ts
+++ test/TaskTry.test.ts
@@ -1,11 +1,34 @@
-export {};
+import * as TaskTry from '../src/TaskTry';
+import * as Try from '../src/Try';
+import * as TaskEither from 'fp-ts/TaskEither';
+import * as Task from 'fp-ts/Task';
+import '@relmify/jest-fp-ts';
 
 describe('TaskTry', () => {
-	it('tryCatch', () => {
-		throw new Error();
+	it('tryCatch', async () => {
+		const successTry: TaskTry.TaskTry<string> = TaskTry.tryCatch(
+			async () => 'Hello'
+		);
+		const failTry: TaskTry.TaskTry<string> = TaskTry.tryCatch(async () => {
+			throw new Error('Dying');
+		});
+
+		const successResult: Try.Try<string> = await successTry();
+		const failResult: Try.Try<string> = await failTry();
+
+		expect(successResult).toEqualRight('Hello');
+		expect(failResult).toEqualLeft(new Error('Dying'));
 	});
 
-	it('TaskTry<T> is interchangeable with TaskEither<Error,T>', () => {
-		throw new Error();
+	it('TaskTry<T> is interchangeable with TaskEither<Error,T>', async () => {
+		const successTry: TaskTry.TaskTry<string> = TaskTry.tryCatch(
+			async () => 'Hello'
+		);
+		const result = await TaskEither.fold(
+			() => Task.of('Failed'),
+			(value) => Task.of(value)
+		)(successTry)();
+
+		expect(result).toEqual('Hello');
 	});
 });
