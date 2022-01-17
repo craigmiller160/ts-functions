8052831f0f9ce40ace73fd530109f8f500533986
Craig <craigmiller160@gmail.com>
2022-01-17 18:44:05 -0500

    Implemented more tests

--- test/File.test.ts
+++ test/File.test.ts
@@ -1,8 +1,10 @@
 import * as File from '../src/File';
 import path from 'path';
 import fs from 'fs';
+import '@relmify/jest-fp-ts';
 
 const TEMP_PATH = path.join(process.cwd(), 'node_modules', 'temp_test_dir');
+const TEXT = 'This is the file text';
 
 describe('File', () => {
 	beforeEach(() => {
@@ -15,19 +17,43 @@ describe('File', () => {
 		fs.rmSync(TEMP_PATH, {
 			recursive: true,
 			force: true
-		})
-	})
+		});
+	});
 
 	it('readFileSync', () => {
-		throw new Error();
+		const filePath = path.join(TEMP_PATH, 'file.txt');
+		fs.writeFileSync(filePath, TEXT);
+		const result = File.readFileSync(filePath);
+		expect(result).toEqualRight(TEXT);
 	});
 
 	it('existsSync', () => {
-		throw new Error();
+		const filePath = path.join(TEMP_PATH, 'file.txt');
+		fs.writeFileSync(filePath, TEXT);
+
+		const result = File.existsSync(() => 'Success')(filePath);
+		expect(result).toEqualSome('Success');
 	});
 
-	it('rmIfExistsSync', () => {
-		throw new Error();
+	it('rmIfExistsSync file', () => {
+		const filePath = path.join(TEMP_PATH, 'file.txt');
+		fs.writeFileSync(filePath, TEXT);
+
+		const result = File.rmIfExistsSync(filePath);
+		expect(result).toBeRight();
+		expect(fs.existsSync(filePath)).toEqual(false);
+	});
+
+	it('rmIfExistsSync directory', () => {
+		const dirPath = path.join(TEMP_PATH, 'dir');
+		const filePath = path.join(dirPath, 'file.txt');
+		fs.mkdirSync(dirPath);
+		fs.writeFileSync(filePath, TEXT);
+
+		const result = File.rmIfExistsSync(dirPath);
+		expect(result).toBeRight();
+		expect(fs.existsSync(filePath)).toEqual(false);
+		expect(fs.existsSync(dirPath)).toEqual(false);
 	});
 
 	it('mkdirSync', () => {
