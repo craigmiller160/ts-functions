490250839ec99d218e7125ebe770fe97ca61135f
Craig Miller <craigmiller160@gmail.com>
2022-01-17 16:17:43 -0500

    Implemented tests

--- test/Json.test.ts
+++ test/Json.test.ts
@@ -1,11 +1,25 @@
-export {};
+import * as Json from '../src/Json';
+import * as Try from '../src/Try';
+import '@relmify/jest-fp-ts';
+
+interface Value {
+	readonly one: string;
+}
 
 describe('Json', () => {
 	it('parse', () => {
-		throw new Error();
+		const text = '{"one":"two"}';
+		const result: Try.Try<Value> = Json.parse<Value>(text);
+		expect(result).toEqualRight({
+			one: 'two'
+		});
 	});
 
 	it('stringify', () => {
-		throw new Error();
+		const value: Value = {
+			one: 'two'
+		};
+		const result: Try.Try<string> = Json.stringify(value);
+		expect(result).toEqualRight('{"one":"two"}');
 	});
 });
