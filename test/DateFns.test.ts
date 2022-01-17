f1b5d7fc68476f5f91fe436972dbe6c33baf91b1
Craig Miller <craigmiller160@gmail.com>
2022-01-17 12:14:28 -0500

    Added DateFns functions and stubbed tests

---
+++ test/DateFns.test.ts
@@ -0,0 +1,19 @@
+export {};
+
+describe('DateFns', () => {
+	it('addMinutes', () => {
+		throw new Error();
+	});
+
+	it('format', () => {
+		throw new Error();
+	});
+
+	it('parse', () => {
+		throw new Error();
+	});
+
+	it('compareAsc', () => {
+		throw new Error();
+	});
+});
