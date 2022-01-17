e5df28035a058481f2cda9fbb26b223a59fbaae7
Craig Miller <craigmiller160@gmail.com>
2022-01-17 11:40:27 -0500

    Added eslint import restriction

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,18 @@ module.exports = {
         '@craigmiller160/eslint-config-prettier',
         '@craigmiller160/eslint-config-jest',
         '@craigmiller160/eslint-config-ts'
-    ]
+    ],
+    rules: {
+        'no-restricted-imports': [
+            'error',
+            {
+                patterns: [
+                    {
+                        group: ['fp-ts/es6/*'],
+                        message: 'Use commonjs fp-ts import'
+                    }
+                ]
+            }
+        ]
+    }
 }
