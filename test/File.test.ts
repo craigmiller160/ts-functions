4f5eaaa9c729478d95c8031475b5e4ace2d4a621
Craig <craigmiller160@gmail.com>
2022-01-17 18:38:50 -0500

    Test setup logic

--- test/File.test.ts
+++ test/File.test.ts
@@ -1,6 +1,23 @@
-export {};
+import * as File from '../src/File';
+import path from 'path';
+import fs from 'fs';
+
+const TEMP_PATH = path.join(process.cwd(), 'node_modules', 'temp_test_dir');
 
 describe('File', () => {
+	beforeEach(() => {
+		fs.mkdirSync(TEMP_PATH, {
+			recursive: true
+		});
+	});
+
+	afterEach(() => {
+		fs.rmSync(TEMP_PATH, {
+			recursive: true,
+			force: true
+		})
+	})
+
 	it('readFileSync', () => {
 		throw new Error();
 	});
