459bdf8955e0c06bf82fbbcc5965c7305e962f46
Craig <craigmiller160@gmail.com>
2022-01-17 08:54:00 -0500

    Stubbed out tests

---
+++ test/Regex.test.ts
@@ -0,0 +1,13 @@
+import '@relmify/jest-fp-ts';
+
+describe('Regex', () => {
+	describe('captureFromRegex', () => {
+		it('has match', () => {
+			throw new Error();
+		});
+
+		it('has no match', () => {
+			throw new Error();
+		});
+	});
+});
