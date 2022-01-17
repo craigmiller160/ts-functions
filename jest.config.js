c9b35489a2dba26fe4811ea481642079a167b85a
Craig <craigmiller160@gmail.com>
2022-01-17 08:48:40 -0500

    Setup lots of config stuff

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+const merge = require('@craigmiller160/config-merge');
+const jestConfig = require('@craigmiller160/jest-config');
+const jestTsConfig = require('@craigmiller160/jest-config-ts');
+const path = require('path');
+
+module.exports = merge(jestConfig, jestTsConfig);
