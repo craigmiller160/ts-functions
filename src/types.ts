e50702e342489db93106b5e48a7e91ffa2758221
Craig <craigmiller160@gmail.com>
2022-01-23 15:48:46 -0500

    Fixed some type signatures

--- src/types.ts
+++ src/types.ts
@@ -3,8 +3,8 @@ import { ReaderTaskEither } from 'fp-ts/ReaderTaskEither';
 
 export type { TaskTry as TaskTryT } from './TaskTry';
 export type { Try as TryT } from './Try';
-export type ReaderTry<D, T> = ReaderEither<D, Error, T>;
-export type ReaderTaskTry<D, T> = ReaderTaskEither<D, Error, T>;
+export type ReaderTryT<D, T> = ReaderEither<D, Error, T>;
+export type ReaderTaskTryT<D, T> = ReaderTaskEither<D, Error, T>;
 export type { Option as OptionT } from 'fp-ts/Option';
 export type { Either as EitherT } from 'fp-ts/Either';
 export type { TaskEither as TaskEitherT } from 'fp-ts/TaskEither';
