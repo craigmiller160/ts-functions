f5124d94c96f2ebb12b86a4433c8cf86255aec2b
Craig <craigmiller160@gmail.com>
2022-01-23 15:35:06 -0500

    Added some more types

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,7 @@
+import { ReaderEither } from 'fp-ts/ReaderEither';
+import { ReaderTaskEither } from 'fp-ts/ReaderTaskEither';
+
 export type { TaskTry as TaskTryT } from './TaskTry';
 export type { Try as TryT } from './Try';
+export type ReaderTry<D, T> = ReaderEither<D, Error, T>;
+export type ReaderTaskTry<D, T> = ReaderTaskEither<D, Error, T>;
