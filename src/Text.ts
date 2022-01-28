f06b0c3cf18205dca917cbe7e07c28f7462c725d
Craig Miller <craigmiller160@gmail.com>
2022-01-28 18:58:44 -0500

    Made split output readonly

--- src/Text.ts
+++ src/Text.ts
@@ -1,6 +1,6 @@
 export const split =
 	(delimiter: string) =>
-	(text: string): string[] =>
+	(text: string): ReadonlyArray<string> =>
 		text.split(delimiter);
 
 export const concat = (delimiter: string) => (s1: string, s2: string) =>
