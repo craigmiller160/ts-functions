66b9545a934467c9be8e1008a5d1ab696ea7be01
Craig Miller <craigmiller160@gmail.com>
2022-02-03 10:16:13 -0500

    Implemented the formatting test and upgraded jest config

--- test/TimeTz.test.ts
+++ test/TimeTz.test.ts
@@ -1,4 +1,4 @@
-import { addMinutes, format } from 'date-fns';
+import { addMinutes, format, set } from 'date-fns';
 import * as TimeTz from '../src/TimeTz';
 
 const TIMESTAMP_FORMAT = 'yyyy-MM-dd HH:mm:ss.SSS';
@@ -21,7 +21,20 @@ describe('TimeTz', () => {
 	});
 
 	it('format', () => {
-		// TODO this will work, above does not
-		throw new Error();
+		const date = set(new Date(), {
+			year: 2020,
+			month: 1,
+			date: 1,
+			hours: 0,
+			minutes: 0,
+			seconds: 0,
+			milliseconds: 0
+		});
+		const expectedUtc = format(
+			addMinutes(date, OFFSET_MINS),
+			TIMESTAMP_FORMAT
+		);
+		const actualUtc = TimeTz.format('UTC')(TIMESTAMP_FORMAT)(date);
+		expect(actualUtc).toEqual(expectedUtc);
 	});
 });
