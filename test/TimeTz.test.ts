c21f5e8523798e980f96a386d26a4ee6e721fbbe
Craig Miller <craigmiller160@gmail.com>
2022-02-02 22:21:37 -0500

    More progress with timezone stuff

--- test/TimeTz.test.ts
+++ test/TimeTz.test.ts
@@ -1,9 +1,27 @@
+import { addMinutes, format } from 'date-fns';
+import * as TimeTz from '../src/TimeTz';
+
+const TIMESTAMP_FORMAT = 'yyyy-MM-dd HH:mm:ss.SSS';
+const CURRENT_TIME_ZONE = Intl.DateTimeFormat().resolvedOptions().timeZone;
+const OFFSET_MINS = new Date().getTimezoneOffset();
+
 describe('TimeTz', () => {
 	it('toUtc and fromUtc', () => {
-		throw new Error();
+		const date = new Date();
+		const expected = format(
+			addMinutes(date, OFFSET_MINS),
+			TIMESTAMP_FORMAT
+		);
+
+		const actual = format(
+			TimeTz.toUtc(CURRENT_TIME_ZONE)(date),
+			TIMESTAMP_FORMAT
+		);
+		expect(actual).toEqual(expected);
 	});
 
 	it('format', () => {
+		// TODO this will work, above does not
 		throw new Error();
 	});
 });
