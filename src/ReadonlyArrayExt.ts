3e8d3f83a03a7650e1ceead34bae640223c62854
Craig Miller <craigmiller160@gmail.com>
2022-02-06 10:59:52 -0500

    Implemented tests

--- src/ReadonlyArrayExt.ts
+++ src/ReadonlyArrayExt.ts
@@ -4,6 +4,9 @@ export const dropFirstMatch =
 	<T>(predicate: PredicateT<T>) =>
 	(arr: ReadonlyArray<T>): ReadonlyArray<T> => {
 		const index = arr.findIndex(predicate);
+		if (index < 0) {
+			return arr;
+		}
 		return arr.slice(0, index).concat(arr.slice(index + 1));
 	};
 
@@ -11,6 +14,9 @@ export const updateFirstMatch =
 	<T>(predicate: PredicateT<T>, updateMatch: (t: T) => T) =>
 	(arr: ReadonlyArray<T>): ReadonlyArray<T> => {
 		const index = arr.findIndex(predicate);
+		if (index < 0) {
+			return arr;
+		}
 		const newItem = updateMatch(arr[index]);
 		const newArr = [...arr];
 		newArr[index] = newItem;
--- test/ReadonlyArrayExt.test.ts
+++ test/ReadonlyArrayExt.test.ts
@@ -1,23 +1,98 @@
-export {};
+import * as RArrayExt from '../src/ReadonlyArrayExt';
+
+interface Person {
+	readonly id: number;
+	readonly name: string;
+	readonly age: number;
+}
+
+const array: ReadonlyArray<Person> = [
+	{
+		id: 1,
+		name: 'Bob',
+		age: 20
+	},
+	{
+		id: 2,
+		name: 'John',
+		age: 30
+	},
+	{
+		id: 3,
+		name: 'Carl',
+		age: 40
+	}
+];
 
 describe('ReadonlyArrayExt', () => {
 	describe('dropFirstMatch', () => {
 		it('drops the match', () => {
-			throw new Error();
+			const result = RArrayExt.dropFirstMatch<Person>((_) => _.id === 2)(
+				array
+			);
+			expect(result).toHaveLength(2);
+			expect(result).toEqual([
+				{
+					id: 1,
+					name: 'Bob',
+					age: 20
+				},
+				{
+					id: 3,
+					name: 'Carl',
+					age: 40
+				}
+			]);
 		});
 
 		it('has no match', () => {
-			throw new Error();
+			const result = RArrayExt.dropFirstMatch<Person>((_) => _.id === 5)(
+				array
+			);
+			expect(result).toHaveLength(3);
+			expect(result).toEqual(array);
 		});
 	});
 
 	describe('updateFirstMatch', () => {
 		it('updates the match', () => {
-			throw new Error();
+			const result = RArrayExt.updateFirstMatch<Person>(
+				(_) => _.id === 2,
+				(_) => ({
+					..._,
+					name: 'Jose'
+				})
+			)(array);
+			expect(result).toHaveLength(3);
+			expect(result).toEqual([
+				{
+					id: 1,
+					name: 'Bob',
+					age: 20
+				},
+				{
+					id: 2,
+					name: 'Jose',
+					age: 30
+				},
+				{
+					id: 3,
+					name: 'Carl',
+					age: 40
+				}
+			]);
 		});
 
 		it('has no match', () => {
-			throw new Error();
+			const result = RArrayExt.updateFirstMatch<Person>(
+				(_) => _.id === 5,
+				(_) => ({
+					..._,
+					name: 'Jose'
+				})
+			)(array);
+			expect(result).toHaveLength(3);
+			expect(result).toEqual(array);
 		});
 	});
 });
