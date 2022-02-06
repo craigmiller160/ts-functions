20c2de5600b3a47924907c72a35a3a8f1db6cce8
Craig Miller <craigmiller160@gmail.com>
2022-02-06 10:51:11 -0500

    Stubbed out tests for new functions

---
+++ test/ReadonlyArrayExt.test.ts
@@ -0,0 +1,23 @@
+export {};
+
+describe('ReadonlyArrayExt', () => {
+	describe('dropFirstMatch', () => {
+		it('drops the match', () => {
+			throw new Error();
+		});
+
+		it('has no match', () => {
+			throw new Error();
+		});
+	});
+
+	describe('updateFirstMatch', () => {
+		it('updates the match', () => {
+			throw new Error();
+		});
+
+		it('has no match', () => {
+			throw new Error();
+		});
+	});
+});
