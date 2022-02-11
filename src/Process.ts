ba93e8e848d2c2fc4669833462e0b54e4fbad8af
Craig Miller <craigmiller160@gmail.com>
2022-02-11 13:55:56 -0500

    Feature/process (#6)
    * Began adding process functions
    
    * Stubbed out tests for more process functions
    
    * Implemented more process functions
    
    * Wrote tests for process functions

---
+++ src/Process.ts
@@ -0,0 +1,26 @@
+import { IOT, OptionT } from './types';
+import { pipe } from 'fp-ts/function';
+import * as IO from 'fp-ts/IO';
+import * as RArray from 'fp-ts/ReadonlyArray';
+import * as Option from 'fp-ts/Option';
+
+export const cwd = (): IOT<string> => () => process.cwd();
+
+export const allRawArgv = (): IOT<ReadonlyArray<string>> => IO.of(process.argv);
+
+export const allUserArgv = (): IOT<ReadonlyArray<string>> =>
+	pipe(IO.of(process.argv), IO.map(RArray.dropLeft(2)));
+
+export const rawArgvLookup = (index: number): IOT<OptionT<string>> =>
+	pipe(allRawArgv(), IO.map(RArray.lookup(index)));
+
+export const userArgvLookup = (index: number): IOT<OptionT<string>> =>
+	pipe(allUserArgv(), IO.map(RArray.lookup(index)));
+
+export const allEnv = (): IOT<NodeJS.ProcessEnv> => IO.of(process.env);
+
+export const envLookup = (key: string): IOT<OptionT<string>> =>
+	pipe(
+		allEnv(),
+		IO.map((env) => Option.fromNullable(env[key]))
+	);
---
+++ test/Process.test.ts
@@ -0,0 +1,47 @@
+import * as Process from '../src/Process';
+import '@relmify/jest-fp-ts';
+
+describe('Process', () => {
+	it('cwd', () => {
+		const expected = process.cwd();
+		const actual = Process.cwd()();
+		expect(actual).toEqual(expected);
+	});
+
+	it('allRawArgv', () => {
+		const expected = process.argv;
+		const actual = Process.allRawArgv()();
+		expect(actual).toEqual(expected);
+	});
+
+	it('allUserArgv', () => {
+		const expected = process.argv.slice(2);
+		const actual = Process.allUserArgv()();
+		expect(actual).toEqual(expected);
+	});
+
+	it('rawArgvLookup', () => {
+		const expected = process.argv[1];
+		const actual = Process.rawArgvLookup(1)();
+		expect(actual).toEqualSome(expected);
+	});
+
+	it('userArgvLookup', () => {
+		const expected = process.argv.slice(2)[1];
+		const actual = Process.userArgvLookup(1)();
+		expect(actual).toEqualSome(expected);
+	});
+
+	it('allEnv', () => {
+		const expected = process.env;
+		const actual = Process.allEnv()();
+		expect(actual).toEqual(expected);
+	});
+
+	it('envLookup', () => {
+		const key = Object.keys(process.env)[0];
+		const expected = process.env[key];
+		const actual = Process.envLookup(key)();
+		expect(actual).toEqualSome(expected);
+	});
+});
