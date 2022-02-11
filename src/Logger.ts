14b2efbf7d502e8729ac7d89c2ba50d3d968fce7
Craig Miller <craigmiller160@gmail.com>
2022-02-11 17:42:53 -0500

    Added a log more logger properties

--- src/Logger.ts
+++ src/Logger.ts
@@ -10,9 +10,23 @@ export interface BaseLogger<R> {
 }
 
 export type ErrorLog<R> = (msg: string, error: Error) => R;
+export type JsonLog<R> = (
+	msg: string,
+	value: object | ReadonlyArray<unknown>
+) => R;
 
 export interface EnhancedLogger<R> extends BaseLogger<R> {
 	readonly errorWithStack: ErrorLog<R>;
+	readonly warnWithStack: ErrorLog<R>;
+	readonly infoWithStack: ErrorLog<R>;
+	readonly debugWithStack: ErrorLog<R>;
+	readonly verboseWithStack: ErrorLog<R>;
+
+	readonly debugWithJson: JsonLog<R>;
+	readonly infoWithJson: JsonLog<R>;
+	readonly warnWithJson: JsonLog<R>;
+	readonly errorWithJson: JsonLog<R>;
+	readonly verboseWithJson: JsonLog<R>;
 }
 
 export type ProvidedLogger = BaseLogger<void>;
