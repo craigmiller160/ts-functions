f0de3aaafd6c3d9fd981e671801909112dfa2efc
Craig Miller <craigmiller160@gmail.com>
2022-02-11 13:41:52 -0500

    Stubbed out tests for more process functions

--- test/Process.test.ts
+++ test/Process.test.ts
@@ -6,4 +6,28 @@ describe('Process', () => {
 		const actual = Process.cwd()();
 		expect(actual).toEqual(expected);
 	});
+
+	it('allRawArgv', () => {
+		throw new Error();
+	});
+
+	it('allUserArgv', () => {
+		throw new Error();
+	});
+
+	it('lookupRawArgv', () => {
+		throw new Error();
+	});
+
+	it('lookupUserArgv', () => {
+		throw new Error();
+	});
+
+	it('allEnv', () => {
+		throw new Error();
+	});
+
+	it('lookupEnv', () => {
+		throw new Error();
+	});
 });
