ee85f6fe52f01ad1bc069cb0a3bd0451d88add23
Craig Miller <craigmiller160@gmail.com>
2022-02-11 08:30:06 -0500

    Working on test setup

--- test/scripts/build.test.ts
+++ test/scripts/build.test.ts
@@ -1,6 +1,19 @@
-export {};
+import { build } from '../../scripts/build';
+import { getCwd } from '../../scripts/getCwd';
+import path from 'path';
+
+jest.mock('../../scripts/getCwd', () => ({
+	getCwd: jest.fn()
+}));
+
+const CWD = path.join(process.cwd(), 'test', 'workingDir');
+const getCwdMock = getCwd as jest.Mock;
 
 describe('build script', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
 	it('builds the artifact with commonjs and esmodule outputs', () => {
 		throw new Error();
 	});
