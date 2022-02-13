81b1c1b45be0cdf71ec32c3cf31d9710bb38dcd0
Craig Miller <craigmiller160@gmail.com>
2022-02-12 19:55:30 -0500

    Fixed tests

--- src/Pattern.ts
+++ src/Pattern.ts
@@ -4,3 +4,23 @@ import { GuardPattern } from 'ts-pattern/lib/types/Pattern';
 
 export const regex = (reg: RegExp | string): GuardPattern<string> =>
 	when(Regex.test(reg));
+
+export const lengthGT = (
+	value: number
+): GuardPattern<string | Array<unknown>> => when((_) => _.length > value);
+
+export const lengthGTE = (
+	value: number
+): GuardPattern<string | Array<unknown>> => when((_) => _.length >= value);
+
+export const lengthEQ = (
+	value: number
+): GuardPattern<string | Array<unknown>> => when((_) => _.length === value);
+
+export const lengthLT = (
+	value: number
+): GuardPattern<string | Array<unknown>> => when((_) => _.length < value);
+
+export const lengthLTE = (
+	value: number
+): GuardPattern<string | Array<unknown>> => when((_) => _.length <= value);
--- test/Pattern.test.ts
+++ test/Pattern.test.ts
@@ -1,17 +1,65 @@
 import { match } from 'ts-pattern';
 import * as Pattern from '../src/Pattern';
+import { GuardPattern } from 'ts-pattern/lib/types/Pattern';
 
 const URL = 'https://www.google.com/foo/bar?abc=def&ghi=jkl';
 const URL_REGEX = /^https?:\/\/(?<hostname>.*?)\/.*\?(?<query>.*)$/;
 
-const doMatchPattern = (value: string): string =>
+const doMatchRegexPattern = (value: string): string =>
 	match(value)
 		.with(Pattern.regex(URL_REGEX), () => 'Success')
 		.otherwise(() => 'Failure');
 
+const lengthPattern =
+	(pattern: (l: number) => GuardPattern<string | Array<unknown>>) =>
+	(length: number) =>
+		match('Hello')
+			.with(pattern(length), () => 'Success')
+			.otherwise(() => 'Failure');
+
 describe('Pattern', () => {
 	it('regex', () => {
-		expect(doMatchPattern(URL)).toEqual('Success');
-		expect(doMatchPattern('abc')).toEqual('Failure');
+		expect(doMatchRegexPattern(URL)).toEqual('Success');
+		expect(doMatchRegexPattern('abc')).toEqual('Failure');
+	});
+
+	it('lengthGT', () => {
+		const fn = lengthPattern(Pattern.lengthGT);
+
+		expect(fn(3)).toEqual('Success');
+		expect(fn(5)).toEqual('Failure');
+		expect(fn(6)).toEqual('Failure');
+	});
+
+	it('lengthGTE', () => {
+		const fn = lengthPattern(Pattern.lengthGTE);
+
+		expect(fn(3)).toEqual('Success');
+		expect(fn(5)).toEqual('Success');
+		expect(fn(6)).toEqual('Failure');
+	});
+
+	it('lengthEQ', () => {
+		const fn = lengthPattern(Pattern.lengthEQ);
+
+		expect(fn(3)).toEqual('Failure');
+		expect(fn(5)).toEqual('Success');
+		expect(fn(6)).toEqual('Failure');
+	});
+
+	it('lengthLT', () => {
+		const fn = lengthPattern(Pattern.lengthLT);
+
+		expect(fn(3)).toEqual('Failure');
+		expect(fn(5)).toEqual('Failure');
+		expect(fn(6)).toEqual('Success');
+	});
+
+	it('lengthLTE', () => {
+		const fn = lengthPattern(Pattern.lengthLTE);
+
+		expect(fn(3)).toEqual('Failure');
+		expect(fn(5)).toEqual('Success');
+		expect(fn(6)).toEqual('Success');
 	});
 });
