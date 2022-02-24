7c98df48a3e66067eba2cf9e78ec9be6ae32f263
Craig Miller <craigmiller160@gmail.com>
2022-02-24 08:15:09 -0500

    Making progress on building new reporter logic

--- src/TypeValidation.ts
+++ src/TypeValidation.ts
@@ -3,8 +3,11 @@ import { identity, pipe } from 'fp-ts/function';
 import * as Either from 'fp-ts/Either';
 import { PathReporter } from 'io-ts/PathReporter';
 import * as ioType from 'io-ts';
-import { Type, ValidationError } from 'io-ts';
+import { Decoder, Type, ValidationError } from 'io-ts';
 import { Reporter } from 'io-ts/Reporter';
+import * as RArray from 'fp-ts/ReadonlyArray';
+import { match, when } from 'ts-pattern';
+import { GuardPattern } from 'ts-pattern/lib/types/Pattern';
 
 export class TypeValidationError extends Error {
 	readonly name = 'TypeValidationError';
@@ -27,7 +30,44 @@ export const handleResult = <T>(result: ValidationT<T>): TryT<T> =>
 		Either.mapLeft((report) => new TypeValidationError(report))
 	);
 
-const createReadableReport = (errors: ReadonlyArray<ValidationError>): ReadonlyArray<string> => {
+enum CurrentEntryType {
+	OBJECT,
+	ARRAY,
+	VALUE
+}
+
+interface ReportPathContext {
+	readonly path: string;
+	readonly currentEntryType: CurrentEntryType;
+}
+
+const startsWith = (value: string): GuardPattern<string> =>
+	when((_) => _.startsWith(value));
+
+const typeToCurrentEntryType = (type: Decoder<any, any>): CurrentEntryType =>
+	match(type.name)
+		.with(startsWith('Array'), () => CurrentEntryType.ARRAY)
+		.with(startsWith('ReadonlyArray'), () => CurrentEntryType.ARRAY)
+		.with(startsWith('Readonly'), () => CurrentEntryType.OBJECT)
+		.otherwise(() => CurrentEntryType.VALUE);
+
+const createErrorMessage = (error: ValidationError): string => {
+	pipe(
+		error.context,
+		RArray.map(
+			(ctx): ReportPathContext => ({
+				path: ctx.key,
+				currentEntryType: typeToCurrentEntryType(ctx.type)
+			})
+		)
+	);
+	return '';
+};
+
+const createReadableReport = (
+	errors: ReadonlyArray<ValidationError>
+): ReadonlyArray<string> => {
+	pipe(errors, RArray.map(createErrorMessage));
 	return [];
 };
 
@@ -36,9 +76,6 @@ export const ReadableReporter: Reporter<ReadonlyArray<string>> = {
 		pipe(
 			result,
 			Either.mapLeft(createReadableReport),
-			Either.fold(
-				identity,
-				() => []
-			)
+			Either.fold(identity, () => [])
 		)
 };
