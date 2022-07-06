55d508357082f157ced4cc90bc7131a76937c0b2
Craig Miller <craigmiller160@gmail.com>
2022-07-06 10:46:25 -0400

    Added setImmediate helper function

--- src/Sleep.ts
+++ src/Sleep.ts
@@ -4,3 +4,6 @@ export const sleep =
 	(millis: number): Task.Task<void> =>
 	() =>
 		new Promise((resolve) => setTimeout(resolve, millis));
+
+export const immediate = (): Task.Task<void> => () =>
+	new Promise((resolve) => setImmediate(resolve));
