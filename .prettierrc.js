343169009c690f296fd11fd5097a01084cce4c2c
Craig Miller <craigmiller160@gmail.com>
2023-09-16 09:57:37 -0400

    Moving to new config

--- .prettierrc.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('@craigmiller160/prettier-config');
--- jest.config.js
+++
@@ -1,4 +0,0 @@
-const jestConfig = require('@craigmiller160/jest-config');
-const path = require('path');
-
-module.exports = jestConfig;
