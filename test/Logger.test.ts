97b81e4066a07a2b1664a31849cf00a4e1a1ea86
Craig Miller <craigmiller160@gmail.com>
2023-09-16 13:59:40 -0400

    Fixed more errors

--- test/Logger.test.ts
+++ test/Logger.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, beforeEach, afterEach } from 'vitest';
+import { describe, it, expect, beforeEach } from 'vitest';
 import { createLogger, Logger, ProvidedLogger } from '../src/Logger';
 
 interface TestProvidedLogger extends ProvidedLogger {
