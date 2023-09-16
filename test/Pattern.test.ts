f1ee6e8641b4f550e8588d6df6f054bd43a33243
Craig Miller <craigmiller160@gmail.com>
2023-09-16 13:55:42 -0400

    Fixed more type errors

--- test/Pattern.test.ts
+++ test/Pattern.test.ts
@@ -2,27 +2,13 @@ import { describe, it, expect } from 'vitest';
 import { match } from 'ts-pattern';
 import * as Pattern from '../src/Pattern';
 
-const URL = 'https://www.google.com/foo/bar?abc=def&ghi=jkl';
-const URL_REGEX = /^https?:\/\/(?<hostname>.*?)\/.*\?(?<query>.*)$/;
-
-const doMatchRegexPattern = (value: string): string =>
-	match(value)
-		.with(Pattern.regex(URL_REGEX), () => 'Success')
-		.otherwise(() => 'Failure');
-
 const lengthPattern =
-	(pattern: (l: number) => GuardP<string | Array<unknown>, boolean>) =>
-	(length: number) =>
+	(pattern: (l: number) => (v: string) => boolean) => (length: number) =>
 		match('Hello')
-			.with(pattern(length), () => 'Success')
+			.when(pattern(length), () => 'Success')
 			.otherwise(() => 'Failure');
 
 describe('Pattern', () => {
-	it('regex', () => {
-		expect(doMatchRegexPattern(URL)).toBe('Success');
-		expect(doMatchRegexPattern('abc')).toBe('Failure');
-	});
-
 	it('lengthGT', () => {
 		const fn = lengthPattern(Pattern.lengthGT);
 
