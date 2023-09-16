e35d3a49737014bf7c05c5e4c92adee516643123
Craig Miller <craigmiller160@gmail.com>
2023-09-16 13:57:50 -0400

    More test import fixes

--- test/File.test.ts
+++ test/File.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach, afterEach } from 'vitest';
 import * as File from '../src/File';
 import path from 'path';
 import fs from 'fs';
--- test/Pattern.test.ts
+++ test/Pattern.test.ts
@@ -1,5 +1,4 @@
 import { describe, it, expect } from 'vitest';
-import { describe, it, expect } from 'vitest';
 import { match } from 'ts-pattern';
 import * as Pattern from '../src/Pattern';
 
