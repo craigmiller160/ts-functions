5d20262b28a0f18b8c9d9a537461c795012e5b74
Craig Miller <craigmiller160@gmail.com>
2023-12-10 14:22:32 -0500

    Updated lib

--- src/unknownToError.ts
+++ src/unknownToError.ts
@@ -2,5 +2,5 @@ import { P, match } from 'ts-pattern';
 
 export const unknownToError = (theUnknown: unknown): Error =>
 	match<unknown, Error>(theUnknown)
-		.with(P.instanceOf(Error), (_) => _ as Error)
+		.with(P.instanceOf(Error), (_) => _)
 		.otherwise((_) => new Error(`Unknown Error: ${_}`));
