9a366b172b5fd7e1ddecd024fc5f543ef9ad7135
Craig Miller <craigmiller160@gmail.com>
2024-01-21 18:16:58 -0500

    Fixing eslint

--- test/TaskTry.test.ts
+++ test/TaskTry.test.ts
@@ -9,9 +9,11 @@ const te: TaskTryT<string> = TaskEither.right('Hello');
 describe('TaskTry', () => {
 	it('tryCatch', async () => {
 		const successTry: TaskTryT<string> = TaskTry.tryCatch(
+			// eslint-disable-next-line @typescript-eslint/require-await
 			async () => 'Hello'
 		);
 
+		// eslint-disable-next-line @typescript-eslint/require-await
 		const failTry: TaskTryT<string> = TaskTry.tryCatch(async () => {
 			throw new Error('Dying');
 		});
@@ -25,6 +27,7 @@ describe('TaskTry', () => {
 
 	it('TaskTry<T> is interchangeable with TaskEither<Error,T>', async () => {
 		const successTry: TaskTryT<string> = TaskTry.tryCatch(
+			// eslint-disable-next-line @typescript-eslint/require-await
 			async () => 'Hello'
 		);
 		const result = await TaskEither.fold(
@@ -37,9 +40,11 @@ describe('TaskTry', () => {
 
 	it('getOrThrow', async () => {
 		const successTry: TaskTryT<string> = TaskTry.tryCatch(
+			// eslint-disable-next-line @typescript-eslint/require-await
 			async () => 'Hello'
 		);
 
+		// eslint-disable-next-line @typescript-eslint/require-await
 		const failTry: TaskTryT<string> = TaskTry.tryCatch(async () => {
 			throw new Error('Dying');
 		});
@@ -59,12 +64,14 @@ describe('TaskTry', () => {
 	describe('chainTryCatch', () => {
 		it('successful promise', async () => {
 			const result = await TaskTry.chainTryCatch(
+				// eslint-disable-next-line @typescript-eslint/require-await
 				async (value: string) => `${value} World`
 			)(te)();
 			expect(result).toEqualRight('Hello World');
 		});
 
 		it('failed promise', async () => {
+			// eslint-disable-next-line @typescript-eslint/require-await
 			const result = await TaskTry.chainTryCatch(async () => {
 				throw new Error('Dying');
 			})(te)();
